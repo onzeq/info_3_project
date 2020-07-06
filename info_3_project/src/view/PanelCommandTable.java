24fdd5486c514fbadb7ab4d52657e61bbf7e1b47
onzeq <onzeq.v@googlemail.com>
2020-07-06 15:28:08 +0200

    message

--- info_3_project/src/view/PanelCommandTable.java
+++ info_3_project/src/view/PanelCommandTable.java
@@ -1,5 +1,5 @@
 package view;
-
+//Importieren der benötigten Bibliotheken
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -20,17 +20,23 @@ import javax.swing.event.ListSelectionListener;
 import model.Command;
 import model.ControlModel;
 
+/**
+ * Legt Panel für Table an
+ * @author jakob
+ *
+ */
 @SuppressWarnings("serial")
 public class PanelCommandTable extends JPanel implements ListSelectionListener{
-	private JButton bRemove = new JButton("Remove");
-	private JButton bUp =   new JButton("Up");
-	private JButton bDown = new JButton("Down");
-	private JButton bStart= new JButton("Start");
-	private JButton bStop = new JButton("Stop");
-	private JLabel JRover = new JLabel("Rover: ");
-	private JTable tCommands = new JTable();
-	ControlUI controlUI = null;
-	ControlModel controlModel = null;
+	//Klassenvariabeln/Komponenten anlegen und zum teil instanzieren
+	private JButton bRemove 		= new JButton("Remove");
+	private JButton bUp 			= new JButton("Up");
+	private JButton bDown 			= new JButton("Down");
+	private JButton bStart			= new JButton("Start");
+	private JButton bStop 			= new JButton("Stop");
+	private JLabel JRover 			= new JLabel("Rover: ");
+	private JTable tCommands 		= new JTable();
+	ControlUI controlUI 			= null;
+	ControlModel controlModel 		= null;
 	TableCommandModel tableCommandModel = null;
 	
 	/**
@@ -44,11 +50,12 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 		controlModel = cm;
 		
 		tableCommandModel = new TableCommandModel(controlModel.getCommandList());
+		//fügt selbst implemenitertes talbeCommandModel der jtable hinzu, geben Liste ihre Struktur vor
 		tCommands.setModel(tableCommandModel);		
+		//weist ListSelectionListener der JTable hinzu um auf Auswahl reagieren zu können
 		tCommands.getSelectionModel().addListSelectionListener(this);
 		this.setView();
 		this.setController();
-		this.setVisible(true);
 		
 	}
 	
@@ -58,6 +65,7 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 		this.add(new JScrollPane(tCommands), BorderLayout.CENTER);
 		
 		JPanel usePanel = new JPanel();
+		//Boxlayout, Anordnung Buttons
 		usePanel.setLayout(new BoxLayout(usePanel, BoxLayout.X_AXIS));
 		usePanel.add(bRemove);
 		usePanel.add(Box.createRigidArea(new Dimension(5,0)));
@@ -77,36 +85,49 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 	private void setController()
 	{
 		
-		
+		//Remove Button
 		bRemove.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent anEvent) 
 			{
+				//nimmt Index von Reihe als Index der Liste, und löscht entsprechendes Element 
 				controlModel.getCommandList().remove(tCommands.getSelectedRow());
+				//aktualisiert Table
 				tableCommandModel.fireTableDataChanged();
+				//default config panel wird sichtbar
 				controlUI.setDefaultConfig();
 				controlUI.messageUpdated("Command Removed successful!");
 			}
 		});
+		//Up Button
 		bUp.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent anEvent) 
 			{
+				//Wenn schon ganz oben, dann Operation nicht möglich
 				if(tCommands.getSelectedRow() == controlModel.getCommandList().getSize()-1)
 				{
+					//default config panel wird sichtbar
 					controlUI.setDefaultConfig();
+					//Message in der MessageArea
 					controlUI.messageUpdated("MoveUp not possible!");
 				}
 				else
 				{
+					//nimmt Index von Reihe als Index der Liste, und moved entsprechendes Element 
 					controlModel.getCommandList().moveUp(tCommands.getSelectedRow());
+					//aktualisiert Table
 					tableCommandModel.fireTableDataChanged();
+					//default config panel wird sichtbar
 					controlUI.setDefaultConfig();
+					//Message in der MessageArea
 					controlUI.messageUpdated("MovedUp successful!");
 				}
 			}
 		});
+		//Down Button
 		bDown.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent anEvent) 
 			{
+				//Wenn schon ganz unten, dann Operation nicht möglich
 				if(tCommands.getSelectedRow() == 0)
 				{
 					controlUI.setDefaultConfig();
@@ -114,6 +135,7 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 				}
 				else
 				{
+					//nimmt Index von Reihe als Index der Liste, und moved entsprechendes Element 
 					controlModel.getCommandList().moveDown(tCommands.getSelectedRow());
 					tableCommandModel.fireTableDataChanged();
 					controlUI.setDefaultConfig();
@@ -122,30 +144,33 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 				
 			}
 		});
+		//Start Button
 		bStart.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent anEvent) 
 			{
+				//startet Simulation
 				controlModel.start();
+				//Benachrichtigt MessageArea
 				controlUI.messageUpdated("Simulation started");
 			}
 		});
+		//Stop Button
 		bStop.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent anEvent) 
 			{
+				//Beendet Simulation
 				controlModel.stop();
+				//Benachrichtigt MessageArea
 				controlUI.messageUpdated("Simulation stopped");
 			}
 		});
-		
-		
-		
 		
 	}
 	
 	
 	
 	/**
-	 * 
+	 * Updated den Table, bekommt Command übergeben
 	 * @param c
 	 */
 	public void updateTable(Command c)
@@ -162,6 +187,7 @@ public class PanelCommandTable extends JPanel implements ListSelectionListener{
 	
 	@Override
 	public void valueChanged(ListSelectionEvent arg0) {
+		//Wenn Zeile nicht verändert wird
 		if(arg0.getValueIsAdjusting() != true)
 		{
 			//happens after moving up or down
